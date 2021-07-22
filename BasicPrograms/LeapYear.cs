ec3eece6450e23dcf8b1cfd0b8c775d7412dcd78
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 21:38:54 +0530

    [Leap Year Program is initialized]

---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class LeapYear
+    {
+        public void FindLeapYear()
+        {
+            Console.WriteLine("Enter the Year Wheather to check Leap or Not :");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 100 == 0))
+                Console.WriteLine(" Entered Year is a Leap year ");
+            else
+                Console.WriteLine(" Entered Year is not a Lead Year ");
+
+
+
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,8 +7,14 @@ namespace BasicPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to  Basic Programs");
+
+            //Calling FlipCoin()
             HeadTail headtail = new HeadTail();
-            headtail.FilpCoin();
+           // headtail.FilpCoin();
+
+            //calling FindLeapYear();
+            LeapYear leapyear = new LeapYear();
+            leapyear.FindLeapYear();
             
         }
     }
