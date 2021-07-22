6d222a53d14b4c4cf68d766c5320cffa428cc2bc
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 21:30:13 +0530

    program refactored

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -7,6 +7,10 @@ namespace BasicPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to  Basic Programs");
+            HeadTail headtail = new HeadTail();
+            headtail.FilpCoin();
+            LeapYear leapyear = new LeapYear();
+           // leapyear.FindLeapYear();
         }
     }
 }
