19658ced1e6dbc850ceaf71de5a8a9d2de6f3f29
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 23:37:25 +0530

    [pavan]:Prime Factors program Initialized

---
+++ BasicPrograms/Factors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicPrograms
+{
+    class Factors
+    {
+        public void PrimeFactor()
+        {
+            Console.WriteLine("Enter of number for harmonic to find their Sum : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < number; i++)//for loop to find prime factors
+            {
+                while (number % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    number = number / i;
+                }
+            }
+            if(number > 2)
+                Console.WriteLine(number);
+        }
+    }
+}
