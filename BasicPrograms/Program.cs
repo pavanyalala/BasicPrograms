109a7948b2f351a6dba0781293dd9ab76bc4d768
pavanyalala <pavanyalala4508@gmail.com>
2021-07-22 23:39:23 +0530

    [pavan]Prime Factors program been initialized

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -10,20 +10,23 @@ namespace BasicPrograms
 
             //Calling FlipCoin()
             HeadTail headtail = new HeadTail();
-            //headtail.FilpCoin();
+            headtail.FilpCoin();
 
             //calling FindLeapYear();
             LeapYear leapyear = new LeapYear();
-            //leapyear.FindLeapYear();
+            leapyear.FindLeapYear();
 
             //calling PowerCalculaton()
             PowerOfTwo poweroftwo = new PowerOfTwo();
-            //poweroftwo.PowerCalculation();
+            poweroftwo.PowerCalculation();
 
             //calling HarmoniNumber()
             Harmonic harmonic = new Harmonic();
             harmonic.HarmonicNumber();
-            
+
+            //calling PrimeFactor()
+            Factors factors = new Factors();
+            factors.PrimeFactor();
         }
     }
 }
